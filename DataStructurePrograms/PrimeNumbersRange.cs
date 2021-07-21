c8f4649d2696999b053daf2796d7e8a5b2be5110
Diwakar <diwakarn1599@gmail.com>
2021-07-21 16:58:56 +0530

    [Diwakar] Pime Anagram and not anagram in 2d array

--- DataStructurePrograms/PrimeNumbersRange.cs
+++ DataStructurePrograms/PrimeNumbersRange.cs
@@ -4,12 +4,14 @@ using System.Text;
 
 namespace DataStructurePrograms
 {
-    class PrimeNumbersRange<T> where T:IComparable
+    class PrimeNumbersRange
     {
         //Initialising array members
         public static int r = 0, ind = 0;
         // 2d - Array for prime numbers 
         public int[,] primeNumbers = new int[10, 100];
+        public int[,] AnagramNumbers = new int[10, 100];
+        public int[,] notAnagramNumbers = new int[10, 100];
 
         /// <summary>
         /// Find prime in range
@@ -34,25 +36,99 @@ namespace DataStructurePrograms
                 }
                 count++;
             }
-           
+            Console.WriteLine("Prime numbers in range");
+            Print(primeNumbers);
+            FindAnagram();
+            Console.WriteLine("Anagram numbers in range");
+            Print(AnagramNumbers);
+            Console.WriteLine("Not Anagram numbers in range");
+            Print(notAnagramNumbers);
         }
 
         /// <summary>
         /// Print the ARRAY
         /// </summary>
-        public void Print()
+        public void Print(int[,] twoDArray)
         {
             for (int i = 0; i < 10; i++)
             {
                 for (int j = 0; j < 100; j++)
                 {
-                    if (primeNumbers[i, j] != 0)
+                    if (twoDArray[i, j] != 0)
                     {
-                        Console.WriteLine(primeNumbers[i, j]);
+                        Console.WriteLine(twoDArray[i, j]);
                     }
                 }
                 Console.WriteLine("***************************************************");
             }
+        }
+
+        public void FindAnagram()
+        {
+            int ind = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    if (primeNumbers[i, j] != 0 && primeNumbers[i, j] > 10)
+                    {
+                        char[] charArr = primeNumbers[i, j].ToString().ToCharArray();
+                        Array.Sort(charArr);
+                        string str1 = new String(charArr);
+                        int firstNum = Convert.ToInt32(str1);
+                        int secondNum = 0,k;
+                        for(k = j + 1; k < 100; k++)
+                        {
+                            char[] charArr1 = primeNumbers[i, k].ToString().ToCharArray();
+                            Array.Sort(charArr1);
+                            string str2 = new String(charArr1);
+                            secondNum = Convert.ToInt32(str2);
+                            if (firstNum == secondNum)
+                            {
+                                break;
+                            }
+                        }
+                        if (firstNum == secondNum)
+                        {
+                            AnagramNumbers[i, ind++] = primeNumbers[i, j];
+                            AnagramNumbers[i, ind++] = primeNumbers[i, k];
+
+                        }
+                       
+                    }
+                }
+               
+            }
+            int flag;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    flag = 0;
+                    if (primeNumbers[i, j] != 0)
+                    {
+                        for (int p = 0; p < 10 && flag!=1; p++)
+                        {
+                            for(int q = 0; q < 100; q++)
+                            {
+                                if (primeNumbers[i, j] == AnagramNumbers[p, q])
+                                {
+                                    flag = 1;
+                                    break;
+                                }
+                            }
+                        }
+                        if (flag == 0)
+                        {
+                            notAnagramNumbers[i, j] = primeNumbers[i, j];
+                            
+                        }
+                    }
+                }
+                
+            }
+
+
         }
         /// <summary>
         /// Find number whether prime or not
