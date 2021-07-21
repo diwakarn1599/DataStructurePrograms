d9e574858b74f3027f23af00b2b780c3c4ca1f19
Diwakar <diwakarn1599@gmail.com>
2021-07-21 18:15:02 +0530

    [Diwakar] Print calendar by getting month and year

---
+++ DataStructurePrograms/Calendar.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+namespace DataStructurePrograms
+{
+    class Calendar
+    {
+        static int year, month; 
+        static int[,] calendar = new int[6, 7];
+        private static DateTime date;
+        
+        public void FindCalendar()
+        {
+            Console.Write("Enter the year? ");
+            year = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the month (January = 1, etc): ");
+            month = Convert.ToInt32(Console.ReadLine());
+            date = new DateTime(year, month, 1);
+           
+            GetCalendar();
+            PrintCalendar();
+        }
+        /// <summary>
+        /// method to find and store calendar in 2d array
+        /// </summary>
+        public void GetCalendar()
+        {
+            int days = DateTime.DaysInMonth(year, month);
+            int currentDay = 1;
+            var dayOfWeek = (int)date.DayOfWeek;
+            for (int i = 0; i < calendar.GetLength(0); i++)
+            {
+                for (int j = 0; j < calendar.GetLength(1) && currentDay - dayOfWeek + 1 <= days; j++)
+                {
+                    if (i == 0 && month > j)
+                    {
+                        calendar[i, j] = 0;
+                    }
+                    else
+                    {
+                        calendar[i, j] = currentDay - dayOfWeek + 1;
+                        currentDay++;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method to print calendar
+        /// </summary>
+        public void PrintCalendar()
+        {
+            Console.WriteLine($"{CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(month)} {year}");
+            Console.WriteLine("Mon Tue Wed Thu Fri Sat Sun");
+
+            for (int i = 0; i < calendar.GetLength(0); i++)
+            {
+                for (int j = 0; j < calendar.GetLength(1); j++)
+                {
+                    if (calendar[i, j] > 0)
+                    {
+                        
+                        if (calendar[i, j] < 10)
+                        {
+                            Console.Write($" {calendar[i, j]}  ");
+                        }
+                        else
+                        {
+                            Console.Write($"{calendar[i, j]}  ");
+                        }
+                    }
+                    else
+                    {
+                        Console.Write("   ");
+                    }
+                }
+                Console.WriteLine("");
+            }
+        }
+
+    }
+}
--- DataStructurePrograms/Program.cs
+++ DataStructurePrograms/Program.cs
@@ -8,7 +8,7 @@ namespace DataStructurePrograms
         {
             Console.WriteLine("Data Structure Programs");
             Console.WriteLine("*******************MENU*******************");
-            Console.WriteLine("1.Unordered List\n2.Ordered List\n3.Balanced Paranthesis\n4.Cash Counter\n5.Palindrome Checker\n6.Hashing Slots\n7.Prime Number in Range\n8.Exit\nEnter Your Option");
+            Console.WriteLine("1.Unordered List\n2.Ordered List\n3.Balanced Paranthesis\n4.Cash Counter\n5.Palindrome Checker\n6.Hashing Slots\n7.Prime Number in Range\n8.Calendar\n9.Exit\nEnter Your Option");
             switch (Convert.ToInt32(Console.ReadLine()))
             {
                 case 1:
@@ -41,6 +41,10 @@ namespace DataStructurePrograms
                     pnr.FindPrimeInRange();
                     break;
                 case 8:
+                    Calendar c = new Calendar();
+                    c.FindCalendar();
+                    break;
+                case 9:
                     Console.WriteLine("Exited");
                     break;
             }
